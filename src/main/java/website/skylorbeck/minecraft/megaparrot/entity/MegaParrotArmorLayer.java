90f2d8e46819497796b14e5ef97299992416d261
drullkus <drullkus@gmail.com>
2024-08-01 11:00:35 -0700

    Fix armor rendering crash

--- src/main/java/website/skylorbeck/minecraft/megaparrot/entity/MegaParrotArmorLayer.java
+++ src/main/java/website/skylorbeck/minecraft/megaparrot/entity/MegaParrotArmorLayer.java
@@ -9,6 +9,7 @@ import net.minecraft.item.HorseArmorItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import software.bernie.geckolib.cache.object.BakedGeoModel;
+import software.bernie.geckolib.model.GeoModel;
 import software.bernie.geckolib.renderer.GeoRenderer;
 import software.bernie.geckolib.renderer.layer.GeoRenderLayer;
 import website.skylorbeck.minecraft.megaparrot.Declarar;
@@ -27,21 +28,23 @@ public class MegaParrotArmorLayer extends GeoRenderLayer<MegaParrotEntity> {
             return;
         }
         HorseArmorItem horseArmorItem = (HorseArmorItem)armorItemStack.getItem();
-        RenderLayer cameo= RenderLayer.getArmorCutoutNoCull(Declarar.getMegaParrotId("textures/entity/armor/"+ horseArmorItem.getEntityTexture().getPath().split("horse_armor_")[1]));
-//        float r = 1f;
-//        float g = 1f;
-//        float b = 1f;
-//        if (horseArmorItem instanceof DyeableHorseArmorItem) {
-//            int m = ((DyeableHorseArmorItem)horseArmorItem).getColor(armorItemStack);
-//            r = (float)(m >> 16 & 0xFF) / 255.0f;
-//            g = (float)(m >> 8 & 0xFF) / 255.0f;
-//            b = (float)(m & 0xFF) / 255.0f;
-//        }
+		RenderLayer cameo = RenderLayer.getArmorCutoutNoCull(Declarar.getMegaParrotId("textures/entity/armor/" + horseArmorItem.getEntityTexture().getPath().split("horse_armor_")[1]));
+        float r = 1f;
+        float g = 1f;
+        float b = 1f;
+        if (horseArmorItem instanceof DyeableHorseArmorItem) {
+            int m = ((DyeableHorseArmorItem)horseArmorItem).getColor(armorItemStack);
+            r = (float)(m >> 16 & 0xFF) / 255.0f;
+            g = (float)(m >> 8 & 0xFF) / 255.0f;
+            b = (float)(m & 0xFF) / 255.0f;
+        }
         poseStack.push();
         //Move or scale the model as you see fit
         poseStack.scale(1.0f, 1.0f, 1.0f);
         poseStack.translate(0.0d, 0.0d, 0.0d);
-        this.getRenderer().defaultRender(poseStack, megaParrot, bufferSource, cameo, bufferIn, megaParrot.getYaw(), partialTicks, packedLightIn);
+
+        GeoModel<MegaParrotEntity> geoModel = getGeoModel();
+        this.getRenderer().actuallyRender(poseStack, megaParrot, geoModel.getBakedModel(geoModel.getModelResource(megaParrot)), cameo, bufferSource, bufferSource.getBuffer(cameo), true, partialTicks, packedLightIn, packedOverlay, r, g, b, 1.0f);
         poseStack.pop();
     }
 }
